39d3ff64c3dd477651ff16831bbbf0de2a1f4baf
abdul-kioum-ahmed-sumon <150023617+abdul-kioum-ahmed-sumon@users.noreply.github.com>
2024-03-26 00:16:16 +0600

    Add files via upload

---
+++ books/add.php
@@ -0,0 +1,109 @@
+<?php
+
+include_once("/Xampp/htdocs/lms/config/config.php");
+include_once(DIR_URL . "include/header.php");
+include_once(DIR_URL . "include/topbar.php");
+include_once(DIR_URL . "include/sidebar.php");
+?>
+
+
+<!--Main content start-->
+
+<main class="mt-4 pt-3">
+    <div class="container-fluid">
+        <!-- cards start-->
+        <div class="row dashbord-counts">
+            <div class="col-md-12">
+                <h4 class="fw-bold text-uppercase">Add Book</h4>
+            </div>
+
+            <div class="col-md-12  mt-4">
+                <div class="card">
+                    <div class="card-header">
+                        Fill the form
+                    </div>
+                    <div class="card-body">
+                        <form>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputEmail1" class="form-label">Book Name</label>
+                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">ISBN Number</label>
+                                        <input type="text" class="form-control" id="exampleInputPassword1">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Publisher Name</label>
+                                        <input type="text" class="form-control" id="exampleInputPassword1">
+                                    </div>
+                                </div>
+
+
+
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Author Name</label>
+                                        <input type="text" class="form-control" id="exampleInputPassword1">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Edition</label>
+                                        <input type="text" class="form-control" id="exampleInputPassword1">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Book Price</label>
+                                        <input type="text" class="form-control" id="exampleInputPassword1">
+                                    </div>
+                                </div>
+
+
+
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Shelf No</label>
+                                        <input type="text" class="form-control" id="exampleInputPassword1">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Category / Course</label>
+                                        <select class="form-control">
+                                            <option value="">1</option>
+                                            <option value="">2</option>
+                                            <option value="">3</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-success">Save</button>
+                                    <button type="reset" class="btn btn-dark">Cancel</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<!--Main content End-->
+
+
+<?php include_once(DIR_URL . "include/footer.php");
+?>
